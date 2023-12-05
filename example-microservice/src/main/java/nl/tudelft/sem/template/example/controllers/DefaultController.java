ba77899e61e811552e2d376c6df6119757bbf816
TudorChirila11 <tudorchirila11@gmail.com>
2023-12-05 14:29:05 +0100

    modified javadoc so pipeline passes

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DefaultController.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DefaultController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Main Controller of the 'Reviews' Microservice
+ * Main Controller of the 'Reviews' Microservice.
  */
 @RestController
 public class DefaultController {
