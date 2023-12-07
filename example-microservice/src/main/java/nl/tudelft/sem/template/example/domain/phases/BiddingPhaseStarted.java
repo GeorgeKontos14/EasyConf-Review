73d70cecd92b97a0bbd385ce457963ae4ec134bc
lpaul <l.b.paul@student.tudelft.nl>
2023-12-07 22:07:21 +0100

    domain events for all the different phase starts and ends

---
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/phases/BiddingPhaseStarted.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.template.example.domain.phases;
+
+import lombok.Getter;
+/**
+ * A DDD domain event that indicated the review phase has started.
+ */
+@Getter
+public class BiddingPhaseStarted {
+    private final int trackId;
+
+    public BiddingPhaseStarted(int trackId) {
+        this.trackId = trackId;
+    }
+}
