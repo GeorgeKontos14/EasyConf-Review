1788e468600a681320f7ff92d18e470523e152c8
Georgios Kontos <kontosgeo04@gmail.com>
2024-01-03 13:52:11 +0200

    Implemented the reviewFindAllReviewsByUserIDGet method.

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/repositories/ReviewRepository.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/repositories/ReviewRepository.java
@@ -4,7 +4,10 @@ import nl.tudelft.sem.template.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
+    List<Review> findReviewByReviewerId(Integer reviewerId);
 }
